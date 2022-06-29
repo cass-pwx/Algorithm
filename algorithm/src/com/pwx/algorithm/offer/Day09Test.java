197ccaa46fb509e5296a3a4562fb72b2f529fb58
pwx <867965365@qq.com>
2022-06-29 21:11:30 +0800

    剑指offer第9天的题目（力扣的学习计划）

---
+++ algorithm/src/com/pwx/algorithm/offer/Day09Test.java
@@ -0,0 +1,58 @@
+package com.pwx.algorithm.offer;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author 彭伟鑫
+ * @date 2022.6.29
+ */
+public class Day09Test {
+
+    @Test
+    public void test1(){}
+}
+
+class Day09Solution {
+
+    /**
+     * 输入一个整型数组，数组中的一个或连续多个整数组成一个子数组。求所有子数组的和的最大值。
+     *
+     * 要求时间复杂度为O(n)。
+     *
+     * 示例1:
+     *
+     * 输入: nums = [-2,1,-3,4,-1,2,1,-5,4]
+     * 输出: 6
+     * 解释:连续子数组[4,-1,2,1] 的和最大，为6。
+     *
+     * @param nums -
+     * @return -
+     */
+    public int maxSubArray(int[] nums) {
+        return -1;
+    }
+
+    /**
+     * 在一个 m*n 的棋盘的每一格都放有一个礼物，每个礼物都有一定的价值（价值大于 0）。
+     * 你可以从棋盘的左上角开始拿格子里的礼物，并每次向右或者向下移动一格、直到到达棋盘的右下角。
+     * 给定一个棋盘及其上面的礼物的价值，请计算你最多能拿到多少价值的礼物？
+     *
+     *
+     * 示例 1:
+     *
+     * 输入:
+     * [
+     *  [1,3,1],
+     *  [1,5,1],
+     *  [4,2,1]
+     * ]
+     * 输出: 12
+     * 解释: 路径 1→3→5→2→1 可以拿到最多价值的礼物
+     *
+     * @param grid -
+     * @return -
+     */
+    public int maxValue(int[][] grid) {
+        return -1;
+    }
+}
