fc1f20757553b1b0a14c89f753b20e1b6fc0cf38
彭伟鑫#A04154 <weixin.peng@casstime.com>
2022-07-01 11:40:34 +0800

    剑指offer第11天的题目（力扣的学习计划）

---
+++ algorithm/src/com/pwx/algorithm/offer/Day11Test.java
@@ -0,0 +1,63 @@
+package com.pwx.algorithm.offer;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author 彭伟鑫#A04154
+ * @date 2022.7.1
+ */
+public class Day11Test {
+
+    @Test
+    public void test1(){}
+}
+
+
+class Day11Solution {
+
+    /**
+     * 给定单向链表的头指针和一个要删除的节点的值，定义一个函数删除该节点。
+     *
+     * 返回删除后的链表的头节点。
+     *
+     * 注意：此题对比原题有改动
+     *
+     * 示例 1:
+     *
+     * 输入: head = [4,5,1,9], val = 5
+     * 输出: [4,1,9]
+     * 解释: 给定你链表中值为5的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
+     * 示例 2:
+     *
+     * 输入: head = [4,5,1,9], val = 1
+     * 输出: [4,5,9]
+     * 解释: 给定你链表中值为1的第三个节点，那么在调用了你的函数之后，该链表应变为 4 -> 5 -> 9.
+     *
+     * @param head -
+     * @param val -
+     * @return -
+     */
+    public ListNode deleteNode(ListNode head, int val) {
+        return null;
+    }
+
+
+    /**
+     * 输入一个链表，输出该链表中倒数第k个节点。为了符合大多数人的习惯，本题从1开始计数，即链表的尾节点是倒数第1个节点。
+     *
+     * 例如，一个链表有 6 个节点，从头节点开始，它们的值依次是 1、2、3、4、5、6。这个链表的倒数第 3 个节点是值为 4 的节点。
+     *
+     * 示例：
+     *
+     * 给定一个链表: 1->2->3->4->5, 和 k = 2.
+     *
+     * 返回链表 4->5.
+     *
+     * @param head -
+     * @param k -
+     * @return -
+     */
+    public ListNode getKthFromEnd(ListNode head, int k) {
+        return null;
+    }
+}
