0e9b4196cc4447a28b338378e30b982b6eceaaae
彭伟鑫#A04154 <weixin.peng@casstime.com>
2022-07-04 09:13:14 +0800

    剑指offer第14天的题目（力扣的学习计划）

---
+++ algorithm/src/com/pwx/algorithm/offer/Day14Test.java
@@ -0,0 +1,67 @@
+package com.pwx.algorithm.offer;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author 彭伟鑫#A04154
+ * @date 2022.7.4
+ */
+public class Day14Test {
+
+    @Test
+    public void test1(){
+
+    }
+}
+
+
+class Solution {
+
+    /**
+     * 给定一个 m x n 二维字符网格 board 和一个字符串单词 word 。如果 word 存在于网格中，返回 true ；否则，返回 false 。
+     *
+     * 单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母不允许被重复使用。
+     *
+     *
+     * 例如，在下面的 3×4 的矩阵中包含单词 "ABCCED"（单词中的字母已标出）。
+     *
+     * 示例 1：
+     *
+     * 输入：board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+     * 输出：true
+     * 示例 2：
+     *
+     * 输入：board = [["a","b"],["c","d"]], word = "abcd"
+     * 输出：false
+     * 
+     * @param board -
+     * @param word -
+     * @return -
+     */
+    public boolean exist(char[][] board, String word) {
+        return false;
+    }
+
+    /**
+     * 地上有一个m行n列的方格，从坐标 [0,0] 到坐标 [m-1,n-1] 。一个机器人从坐标 [0, 0] 的格子开始移动，
+     * 它每次可以向左、右、上、下移动一格（不能移动到方格外），也不能进入行坐标和列坐标的数位之和大于k的格子。
+     * 例如，当k为18时，机器人能够进入方格 [35, 37] ，因为3+5+3+7=18。但它不能进入方格 [35, 38]，因为3+5+3+8=19。请问该机器人能够到达多少个格子？
+     *
+     * 示例 1：
+     *
+     * 输入：m = 2, n = 3, k = 1
+     * 输出：3
+     * 示例 2：
+     *
+     * 输入：m = 3, n = 1, k = 0
+     * 输出：1
+     *
+     * @param m -
+     * @param n -
+     * @param k -
+     * @return -
+     */
+    public int movingCount(int m, int n, int k) {
+        return -1;
+    }
+}
