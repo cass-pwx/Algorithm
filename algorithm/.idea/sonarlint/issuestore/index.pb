417157ef8f8041e8c40881d2b2f5579bd7407c93
彭伟鑫#A04154 <weixin.peng@casstime.com>
2022-07-06 09:11:19 +0800

    剑指offer第16天的题目（力扣的学习计划）

---
+++ algorithm/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+Z
+*src/com/pwx/algorithm/offer/Day02Test.java,e\f\ef6221ec5dd9fef11baa324f9f0c2ede9c21fee6
---
+++ algorithm/src/com/pwx/algorithm/offer/Day16Test.java
@@ -0,0 +1,66 @@
+package com.pwx.algorithm.offer;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author 彭伟鑫#A04154
+ * @date 2022.7.6
+ */
+public class Day16Test {
+
+    @Test
+    public void test1(){}
+}
+
+class Day16Solution {
+
+    /**
+     * 输入一个非负整数数组，把数组里所有数字拼接起来排成一个数，打印能拼接出的所有数字中最小的一个。
+     *
+     * 示例 1:
+     *
+     * 输入: [10,2]
+     * 输出: "102"
+     * 示例 2:
+     *
+     * 输入: [3,30,34,5,9]
+     * 输出: "3033459"
+     *
+     * 提示:
+     *
+     * 0 < nums.length <= 100
+     *
+     * @param nums -
+     * @return -
+     */
+    public String minNumber(int[] nums) {
+        return null;
+    }
+
+    /**
+     * 从若干副扑克牌中随机抽 5 张牌，判断是不是一个顺子，即这5张牌是不是连续的。
+     * 2～10为数字本身，A为1，J为11，Q为12，K为13，而大、小王为 0 ，可以看成任意数字。A 不能视为 14。
+     *
+     * 示例1:
+     *
+     * 输入: [1,2,3,4,5]
+     * 输出: True
+     *
+     * 示例 2:
+     *
+     * 输入: [0,0,1,2,5]
+     * 输出: True
+     *
+     * 限制：
+     *
+     * 数组长度为 5
+     *
+     * 数组的数取值为 [0, 13] .
+     *
+     * @param nums -
+     * @return -
+     */
+    public boolean isStraight(int[] nums) {
+        return false;
+    }
+}
