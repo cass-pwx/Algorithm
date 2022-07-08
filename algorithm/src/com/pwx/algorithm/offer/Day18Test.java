e600b1ee09319994867e060fd733308cb13f4ec5
彭伟鑫#A04154 <weixin.peng@casstime.com>
2022-07-08 09:25:25 +0800

    剑指offer第18天的题目（力扣的学习计划）

---
+++ algorithm/src/com/pwx/algorithm/offer/Day18Test.java
@@ -0,0 +1,75 @@
+package com.pwx.algorithm.offer;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author 彭伟鑫#A04154
+ * @date 2022.7.8
+ */
+public class Day18Test {
+
+    @Test
+    public void test1(){}
+}
+
+class Solution {
+
+    /**
+     * 输入一棵二叉树的根节点，求该树的深度。
+     * 从根节点到叶节点依次经过的节点（含根、叶节点）形成树的一条路径，最长路径的长度为树的深度。
+     *
+     * 例如：
+     *
+     * 给定二叉树 [3,9,20,null,null,15,7]，
+     *
+     *     3
+     *    / \
+     *   9  20
+     *     /  \
+     *    15   7
+     * 返回它的最大深度 3 。
+     *
+     * @param root -
+     * @return -
+     */
+    public int maxDepth(TreeNode root) {
+        return -1;
+    }
+
+    /**
+     * 输入一棵二叉树的根节点，判断该树是不是平衡二叉树。如果某二叉树中任意节点的左右子树的深度相差不超过1，那么它就是一棵平衡二叉树。
+     *
+     *  
+     *
+     * 示例 1:
+     *
+     * 给定二叉树 [3,9,20,null,null,15,7]
+     *
+     *     3
+     *    / \
+     *   9  20
+     *     /  \
+     *    15   7
+     * 返回 true 。
+     *
+     * 示例 2:
+     *
+     * 给定二叉树 [1,2,2,3,3,null,null,4,4]
+     *
+     *        1
+     *       / \
+     *      2   2
+     *     / \
+     *    3   3
+     *   / \
+     *  4   4
+     * 返回 false 。
+     *
+     * @param root -
+     * @return -
+     */
+    public boolean isBalanced(TreeNode root) {
+        return false;
+    }
+}
+
