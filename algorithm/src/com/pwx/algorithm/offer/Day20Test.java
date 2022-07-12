8ae32f4ac268bba13348978159f452023c8d1553
pwx <867965365@qq.com>
2022-07-12 23:50:35 +0800

    剑指offer第20天的题目（力扣的学习计划）

--- algorithm/src/com/pwx/algorithm/offer/Day20Test.java
+++ algorithm/src/com/pwx/algorithm/offer/Day20Test.java
@@ -2,6 +2,11 @@ package com.pwx.algorithm.offer;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
 /**
  * @author 彭伟鑫#A04154
  * @date 2022.7.11
@@ -17,6 +22,7 @@ public class Day20Test {
 
 class Day20Solution {
 
+    private Map<Integer, Integer> indexMap;
     /**
      * 输入某二叉树的前序遍历和中序遍历的结果，请构建该二叉树并返回其根节点。
      *
@@ -41,7 +47,61 @@ class Day20Solution {
      * @return -
      */
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return null;
+        if (preorder == null || preorder.length == 0) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[0]);
+        Deque<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
+        int inorderIndex = 0;
+        for (int i = 1; i < preorder.length; i++) {
+            int preorderVal = preorder[i];
+            TreeNode node = stack.peek();
+            if (node.val != inorder[inorderIndex]) {
+                node.left = new TreeNode(preorderVal);
+                stack.push(node.left);
+            } else {
+                while (!stack.isEmpty() && stack.peek().val == inorder[inorderIndex]) {
+                    node = stack.pop();
+                    inorderIndex++;
+                }
+                node.right = new TreeNode(preorderVal);
+                stack.push(node.right);
+            }
+        }
+        return root;
+    }
+
+    public TreeNode myBuildTree(int[] preorder, int[] inorder, int preorderLeft,
+                                int preorderRight, int inorderLeft, int inorderRight) {
+        if (preorderLeft > preorderRight) {
+            return null;
+        }
+
+        // 前序遍历中的第一个节点就是根节点
+        // 在中序遍历中定位根节点
+        int inorderRoot = indexMap.get(preorder[preorderLeft]);
+        // 先把根节点建立出来
+        TreeNode root = new TreeNode(preorder[preorderLeft]);
+        // 得到左子树中的节点数目
+        int sizeLeftSubtree = inorderRoot - inorderLeft;
+        // 递归地构造左子树，并连接到根节点
+        // 先序遍历中「从 左边界+1 开始的 size_left_subtree」个元素就对应了中序遍历中「从 左边界 开始到 根节点定位-1」的元素
+        root.left = myBuildTree(preorder, inorder, preorderLeft + 1, preorderLeft + sizeLeftSubtree, inorderLeft, inorderRoot - 1);
+        // 递归地构造右子树，并连接到根节点
+        // 先序遍历中「从 左边界+1+左子树节点数目 开始到 右边界」的元素就对应了中序遍历中「从 根节点定位+1 到 右边界」的元素
+        root.right = myBuildTree(preorder, inorder, preorderLeft + sizeLeftSubtree + 1, preorderRight, inorderRoot + 1, inorderRight);
+        return root;
+    }
+
+    public TreeNode buildTree1(int[] preorder, int[] inorder) {
+        int n = preorder.length;
+        // 构造哈希映射，帮助我们快速定位根节点
+        indexMap = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            indexMap.put(inorder[i], i);
+        }
+        return myBuildTree(preorder, inorder, 0, n - 1, 0, n - 1);
     }
 
     /**
@@ -151,6 +211,7 @@ class Day20Solution {
      * @return -
      */
     public boolean verifyPostorder(int[] postorder) {
+
         return false;
     }
 }
