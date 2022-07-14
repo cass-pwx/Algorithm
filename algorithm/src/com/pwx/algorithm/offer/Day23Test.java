aa4f60db3912eed7756b0be8a76fcd6dc60a6848
彭伟鑫#A04154 <weixin.peng@casstime.com>
2022-07-14 16:46:11 +0800

    剑指offer第23,24天的题目（力扣的学习计划）

---
+++ algorithm/src/com/pwx/algorithm/offer/Day23Test.java
@@ -0,0 +1,59 @@
+package com.pwx.algorithm.offer;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author 彭伟鑫#A04154
+ * @date 2022.7.14
+ */
+public class Day23Test {
+
+    @Test
+    public void test1(){}
+}
+
+class Day23Solution {
+
+    /**
+     * 数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。
+     *
+     * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+     *
+     * 示例 1:
+     *
+     * 输入: [1, 2, 3, 2, 2, 2, 5, 4, 2]
+     * 输出: 2
+     *
+     * 限制：
+     *
+     * 1 <= 数组长度 <= 50000
+     *
+     * @param nums -
+     * @return -
+     */
+    public int majorityElement(int[] nums) {
+        return -1;
+    }
+
+    /**
+     * 给定一个数组 A[0,1,…,n-1]，请构建一个数组 B[0,1,…,n-1]，其中 B[i] 的值是数组 A 中除了下标 i 以外的元素的积,
+     * 即 B[i]=A[0]×A[1]×…×A[i-1]×A[i+1]×…×A[n-1]。不能使用除法。
+     *
+     * 示例:
+     *
+     * 输入: [1,2,3,4,5]
+     * 输出: [120,60,40,30,24]
+     *  
+     *
+     * 提示：
+     *
+     * 所有元素乘积之和不会溢出 32 位整数
+     * a.length <= 100000
+     *
+     * @param a -
+     * @return -
+     */
+    public int[] constructArr(int[] a) {
+        return new int[0];
+    }
+}
