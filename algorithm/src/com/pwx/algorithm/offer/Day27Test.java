904f4feab309999a19dedec7a42684b01e493bd7
pwx <867965365@qq.com>
2022-07-18 23:32:17 +0800

    剑指offer第26,27,28天的题目（力扣的学习计划）

---
+++ algorithm/src/com/pwx/algorithm/offer/Day27Test.java
@@ -0,0 +1,95 @@
+package com.pwx.algorithm.offer;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author 彭伟鑫
+ * @date 2022.7.18
+ */
+public class Day27Test {
+
+    @Test
+    public void test1() {
+    }
+
+}
+
+class Day27Solution {
+
+    /**
+     * 给定一个数组 nums 和滑动窗口的大小 k，请找出所有滑动窗口里的最大值。
+     * <p>
+     * 示例:
+     * <p>
+     * 输入: nums = [1,3,-1,-3,5,3,6,7], 和 k = 3
+     * 输出: [3,3,5,5,6,7]
+     * 解释:
+     * <p>
+     * 滑动窗口的位置                最大值
+     * ---------------               -----
+     * [1  3  -1] -3  5  3  6  7       3
+     * 1 [3  -1  -3] 5  3  6  7       3
+     * 1  3 [-1  -3  5] 3  6  7       5
+     * 1  3  -1 [-3  5  3] 6  7       5
+     * 1  3  -1  -3 [5  3  6] 7       6
+     * 1  3  -1  -3  5 [3  6  7]      7
+     * <p>
+     * <p>
+     * 提示：
+     * <p>
+     * 你可以假设 k 总是有效的，在输入数组不为空的情况下，1 ≤ k ≤ 输入数组的大小。
+     *
+     * @param nums -
+     * @param k    -
+     * @return -
+     */
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int[] result = new int[0];
+        return result;
+    }
+}
+
+
+/**
+ * 请定义一个队列并实现函数 max_value 得到队列里的最大值，
+ * 要求函数max_value、push_back 和 pop_front 的均摊时间复杂度都是O(1)。
+ * <p>
+ * 若队列为空，pop_front 和 max_value 需要返回 -1
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入:
+ * ["MaxQueue","push_back","push_back","max_value","pop_front","max_value"]
+ * [[],[1],[2],[],[],[]]
+ * 输出: [null,null,null,2,1,2]
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入:
+ * ["MaxQueue","pop_front","max_value"]
+ * [[],[],[]]
+ * 输出: [null,-1,-1]
+ * <p>
+ * 限制：
+ * <p>
+ * 1 <= push_back,pop_front,max_value的总操作数 <= 10000
+ * 1 <= value <= 10^5
+ */
+class MaxQueue {
+
+    public MaxQueue() {
+
+    }
+
+    public int max_value() {
+        return -1;
+    }
+
+    public void push_back(int value) {
+
+    }
+
+    public int pop_front() {
+        return -1;
+    }
+}
---
+++ algorithm/src/com/pwx/algorithm/offer/Day28Test.java
@@ -0,0 +1,74 @@
+package com.pwx.algorithm.offer;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author 彭伟鑫
+ * @date 2022.7.18
+ */
+public class Day28Test {
+
+    @Test
+    public void test1() {
+    }
+
+}
+
+class Day28Solution {
+
+    /**
+     * 输入一个字符串，打印出该字符串中字符的所有排列。
+     * <p>
+     * 你可以以任意顺序返回这个字符串数组，但里面不能有重复元素。
+     * <p>
+     * 示例:
+     * <p>
+     * 输入：s = "abc"
+     * 输出：["abc","acb","bac","bca","cab","cba"]
+     * <p>
+     * 限制：
+     * <p>
+     * 1 <= s 的长度 <= 8
+     *
+     * @param s -
+     * @return -
+     */
+    public String[] permutation(String s) {
+        return new String[0];
+    }
+}
+
+/**
+ * 请实现两个函数，分别用来序列化和反序列化二叉树。
+ * <p>
+ * 你需要设计一个算法来实现二叉树的序列化与反序列化。
+ * 这里不限定你的序列 / 反序列化算法执行逻辑，你只需要保证一个二叉树可以被序列化为一个字符串并且将这个字符串反序列化为原始的树结构。
+ * <p>
+ * 提示：输入输出格式与 LeetCode 目前使用的方式一致，详情请参阅 LeetCode 序列化二叉树的格式。
+ * 你并非必须采取这种方式，你也可以采用其他的方法解决这个问题。
+ * <p>
+ * 输入：root = [1,2,3,null,null,4,5]
+ * 输出：[1,2,3,null,null,4,5]
+ */
+class Codec {
+
+    /**
+     * Encodes a tree to a single string.
+     *
+     * @param root -
+     * @return -
+     */
+    public String serialize(TreeNode root) {
+        return null;
+    }
+
+    /**
+     * Decodes your encoded data to tree.
+     *
+     * @param data -
+     * @return -
+     */
+    public TreeNode deserialize(String data) {
+        return null;
+    }
+}
